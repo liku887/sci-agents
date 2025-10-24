dcec1f7b2435f921dc6a8004c3ed1265cfafca2c
youngze07 <870678294@qq.com>
2025-10-24 21:20:44 +0800

    Update index.tsx

--- app/components/chat/index.tsx
+++ app/components/chat/index.tsx
@@ -217,8 +217,8 @@ const Chat: FC<IChatProps> = ({
                 onKeyDown={handleKeyDown}
                 autoSize
               />
-              <div className="absolute bottom-2 right-6 flex items-center h-8">
-                <div className={`${s.count} mr-3 h-5 leading-5 text-sm bg-gray-50 text-gray-500 px-2 rounded`}>{query.trim().length}</div>
+              <div className="absolute bottom-3 right-3 flex items-end h-8">
+                <div className={`${s.count} mr-3 h-6 leading-6 flex items-center text-sm bg-gray-50 text-gray-500 px-2 rounded`}>{query.trim().length}</div>
                 <Tooltip
                   selector='send-tip'
                   htmlContent={
